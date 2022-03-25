5f3f8390f280a7c10097a2bbd0091b6bb93e958b
rajiololade <ololaderaji2011@gmailcom>
2022-03-25 12:05:00 +0000

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 /**
  *main - entry point
  *
- *return: 0 after printing the function
+ *Return: 0 after printing the function
+ *
  */
-{
 int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }
