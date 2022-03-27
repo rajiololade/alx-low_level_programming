468b1576119422b1d78224bc136a6c9c51fe2289
rajiololade <ololaderaji2011@gmail.com>
2022-03-27 14:58:12 -0700

    Mixed alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,17 +9,17 @@ int main(void)
 	char small_letter;
 	char capital_letter;
 
-		for (small_letter == 'a'; small_letter <= 'z'; small_letter++)
+		for (small_letter = 'a'; small_letter <= 'z'; small_letter++)
 			
 		{
 			putchar(small_letter);
 		}
 
-		for (capita_letter == 'A'; capital_letter <= 'Z'; capital_letter++)
+		for (capital_letter = 'A'; capital_letter <= 'Z'; capital_letter++)
 
 		{
 			putchar(capital_letter);
 		}
 
-		putchar('\n');
+	putchar('\n');
 }
