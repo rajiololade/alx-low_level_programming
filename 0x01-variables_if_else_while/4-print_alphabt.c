556742b007652de2467dc5ccea0d9bd75b6c7764
rajiololade <ololaderaji2011@gmail.com>
2022-03-27 15:42:10 -0700

    my commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: 0 Always (Success/correct)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet == 'a'; alphabet <='z'; alphabet++)
+	
+	{
+		if (alphabet == 'q')
+		
+		continue;
+
+		else if (alphabet == 'e')
+
+		continue;
+
+	putchar(alphabet);
+	}
+
+	putchar('\n');
+
+return (0);
+}
+		
+		
