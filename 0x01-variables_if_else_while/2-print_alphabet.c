f421e2c2e781650d1488fcdbdce06ef11a3eab29
rajiololade <ololaderaji2011@gmailcom>
2022-03-27 16:36:53 +0000

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ *Main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+	for (alphabet = 'a', alphabet <= 'z', alphabet++)
+	{
+		putchar(alphabet)
+	}
+		putchar('\n');
+	return (0);
+}
