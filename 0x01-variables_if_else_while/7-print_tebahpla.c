5b7e6f74c6acf051d588e28975fc914023b93753
rajiololade <ololaderaji2011@gmail.com>
2023-02-18 02:03:08 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void) /* The code should return an integer*/
+
+{
+	char alphabet; /* Declaring the character*/
+
+	for (alphabet = 'z'; alphabet <= 'a'; alphabet--)
+
+	{
+		putchar(alphabet); /*main function*/
+	}
+		putchar('\n');
+
+	return (0);
+}
